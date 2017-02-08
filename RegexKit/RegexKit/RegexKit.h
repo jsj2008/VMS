a1c9391217fdc0fad71f9ea00896bf977f2727bd
mac_dev <mac_dev@mac-devdeiMac.local>
2017-02-08 15:54:34 +0800

    RegexKit

---
+++ RegexKit/RegexKit/RegexKit.h
@@ -0,0 +1,19 @@
+//
+//  RegexKit.h
+//  RegexKit
+//
+//  Created by mac_dev on 15/9/15.
+//  Copyright (c) 2015年 mac_dev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RegexKit.
+FOUNDATION_EXPORT double RegexKitVersionNumber;
+
+//! Project version string for RegexKit.
+FOUNDATION_EXPORT const unsigned char RegexKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RegexKit/PublicHeader.h>
+
+
